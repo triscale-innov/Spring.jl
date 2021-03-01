7629223a552ac91fc9883637fc2ce5c45d4c4b78
Laurent Plagne <laurent.plagne@triscale-innov.com>
2021-03-01 14:14:38 +0100

    Add MyStruct

--- src/Spring.jl
+++ src/Spring.jl
@@ -1,3 +1,21 @@
 module Spring
 
+using Plots
+toto(x) = cos(x)
+
+struct MyStruct
+    myfield::Float32
+end
+
+
+function go()
+    x=[Float64(i) for i ∈ 1:40]
+    y=toto.(x)
+    plot(x,y)
+
+    mf=MyStruct(3)
+    @show mf
+
+end
+
 end # module
